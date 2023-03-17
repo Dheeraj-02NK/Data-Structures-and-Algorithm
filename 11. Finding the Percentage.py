2679975f4086c3dc79392a35f0490588b8638090
Dheeraj N Kashyap <95096072+Dheeraj-02NK@users.noreply.github.com>
2023-03-17 23:25:16 +0530

    Create 11. Finding the Percentage.py

---
+++ 11. Finding the Percentage.py
@@ -0,0 +1,18 @@
+The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
+Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+CODE:
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    marks = student_marks[query_name]
+    print(format(sum(marks)/3,'.2f'))
+    
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
