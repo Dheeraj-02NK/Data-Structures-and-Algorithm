35b4cb96777ef62bb78e21b30840439f6598b542
Dheeraj N Kashyap <95096072+Dheeraj-02NK@users.noreply.github.com>
2023-10-26 23:12:48 +0530

    Add files via upload

---
+++ 06. Trees/02. Binary Tree imp.py
@@ -0,0 +1,23 @@
+class BinarySearchTreeNode:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            # add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            # add data in right subtree
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
