1d88347a11d93dad4750722315657a8adda00ad1
Dheeraj N Kashyap <95096072+Dheeraj-02NK@users.noreply.github.com>
2023-10-27 13:25:05 +0530

    Add files via upload

---
+++ 06. Trees/04. Binary search tree.py
@@ -0,0 +1,69 @@
+class BinarySearchTreeNode:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+        if data < self.data:
+            # add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            # add data in right subtree
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elements = []
+        # visit left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+        # visit base node
+        elements.append(self.data)
+        # visit right node
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def search(self, val):
+        if self.data == val:
+            return True
+        if val < self.data:
+            # value might be in left tree
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+        if val > self.data:
+            # value might be in right tree
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    numbers = []
+    size = int(input("Enter number of elements: "))
+    print("Enter ",size,"elements: ")
+    for i in range(size):
+        num = int(input())
+        numbers.append(num)
+    number_tree = build_tree(numbers)
+
+    val_to_search = int(input("Enter the element to search: "))
+    print(number_tree.search(val_to_search))
